d83509e62b089346d8630b9ac82bd55cff25926f
Elena Belova <vitta.dev@gmail.com>
2016-11-11 13:04:20 +0200

    fixed setup

--- setup.py
+++ setup.py
@@ -14,13 +14,16 @@ setup(
     include_package_data=True,
     zip_safe=False,  # because we're including media that Django needs
     install_requires=[
-        # 'django-facebook-api>=0.6.2',
+        'django-facebook-api>=0.6.2',
         'django-facebook-applications>=0.1.0',
         'django-facebook-users>=0.3.0',
         'django-facebook-pages>=0.3.0',
         'django-m2m-history>=0.1.2',
     ],
-    dependency_links=['https://github.com/vittadev/django-facebook-api#egg=django-facebook-api-0.6.2'],
+    dependency_links=[
+        'https://github.com/vittadev/django-facebook-api#egg=django-facebook-api',
+        'https://github.com/vittadev/django-facebook-applications#egg=django-facebook-applications',
+                      ],
     classifiers=[
         'Development Status :: 4 - Beta',
         'Environment :: Web Environment',
